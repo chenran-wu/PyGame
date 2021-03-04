12830b5c626ded5b4f6c37d1a38ccc57d1f4abcb
chenran-wu <63807112+chenran-wu@users.noreply.github.com>
2021-03-04 16:56:23 +0200

    icon

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 pygame.init()
 screen = pygame.display.set_mode([800, 600])
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
 running = True
 while running:
     for event in pygame.event.get():
