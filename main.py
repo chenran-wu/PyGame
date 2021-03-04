cafd72716825c5b22b1bd4ed08e9008f7c86477a
chenran-wu <63807112+chenran-wu@users.noreply.github.com>
2021-03-04 19:44:33 +0200

    move player (fix that bug)

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
 
 
 def player(x, y):
@@ -20,6 +21,14 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill((0, 0, 0))
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -1.2
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 1.2
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+    playerX += playerX_change
     player(playerX, playerY)
     pygame.display.update()
