a095c5f7cd47b85fa0bfa48f4bc9f36bc71d8d8e
chenran-wu <63807112+chenran-wu@users.noreply.github.com>
2021-03-05 16:21:25 +0200

    never change X

--- main.py
+++ main.py
@@ -56,8 +56,9 @@ while running:
             if event.key == pygame.K_RIGHT:
                 playerX_change = 5
             if event.key == pygame.K_SPACE:
-                bulletX = playerX
-                fire_bullet(bulletX, bulletY)
+                if bullet_state is "ready":
+                    bulletX = playerX
+                    fire_bullet(bulletX, bulletY)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
