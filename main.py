7e8aa221204f397b51314577617183cb711b4c55
chenran-wu <63807112+chenran-wu@users.noreply.github.com>
2021-03-05 16:41:17 +0200

    kill enemy

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 import pygame
@@ -28,6 +29,8 @@ bulletX_change = 0
 bulletY_change = 10
 bullet_state = "ready"
 
+score = 0
+
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
@@ -41,6 +44,14 @@ def fire_bullet(x, y):
     global bullet_state
     bullet_state = "fire"
     screen.blit(bulletImg, (x + 16, y + 10))
+
+
+def isCollision(enemyX, enemyY, bulletX, bulletY):
+    distance = math.sqrt(math.pow(enemyX - bulletX, 2) + math.pow(enemyY - bulletY, 2))
+    if distance < 27:
+        return True
+    else:
+        return False
 
 
 running = True
@@ -82,6 +93,15 @@ while running:
     if bullet_state is "fire":
         fire_bullet(bulletX, bulletY)
         bulletY -= bulletY_change
+    collision = isCollision(enemyX, enemyY, bulletX, bulletY)
+    if collision:
+        bulletY = 480
+        bullet_state = "ready"
+        score += 1
+        print(score)
+        enemyX = random.randint(0, 800)
+        enemyY = random.randint(50, 250)
+        
     player(playerX, playerY)
     enemy(enemyX, enemyY)
     pygame.display.update()
